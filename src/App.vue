38ad0ec93c60690d6d0184eb747c7dc4057d5884
Julia Delis <juliadelis2@hotmail.com>
2025-11-05 14:52:08 -0300

    feat: project preparation

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template></template>
+
+<script>
+import employeeData from "./data/purple_cross_employees.json";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      employeesData: employeeData,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
